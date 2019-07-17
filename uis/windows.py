8e4e96702450dea94e750071776562f5b51504e7
StarikTenger <48261272+StarikTenger@users.noreply.github.com>
2019-07-18 00:07:39 +0300

    windows fixed

--- uis/windows.py
+++ uis/windows.py
@@ -3,7 +3,7 @@ from config import config
 from PIL import Image
 
 
-def on_clicked(item):
+def on_clicked(icon, item):
     title = 'mouse' if item.text == 'Мышь' else 'keyboard'
     config.set_value(title, not config.get(title))
 
@@ -11,7 +11,7 @@ def on_clicked(item):
 def run():
     # TODO: Решить проблемы с отметками
 
-    Icon('Quantify', Image.open('assets/icon_win.png'), menu=Menu(
-        MenuItem('Мышь', on_clicked, checked=lambda x: config.get('mouse')),
-        MenuItem('Клавиатура', on_clicked, checked=lambda x: config.get('keyboard'))
+    Icon('Quantify', Image.open('../assets/icon_win.png'), menu=Menu(
+        MenuItem(text='Мышь', action=on_clicked, checked=lambda x: config.get('mouse')),
+        MenuItem(text='Клавиатура', action=on_clicked, checked=lambda x: config.get('keyboard'))
     )).run()
