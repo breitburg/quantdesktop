331f148f9737dc391b5842e77824662c77a2a761
breitburg <contact@breitburg.me>
2019-07-18 00:30:20 +0300

    Final changes

--- main.py
+++ main.py
@@ -10,23 +10,27 @@ basicConfig(level=INFO)
 
 def server_pull(is_alive):
     while is_alive:
+        info('Новая итерация цикла...')
         for item in to_load:
             if config.get(item.name):
+                info(f'Обновляем данные для {item.name}')
                 item.update()
-        sleep(2)
+        info('Ожидание...')
+        sleep(10)
 
 
-is_alive = True
-puller_thread = Thread(target=server_pull, args=(is_alive, ))
-puller_thread.start()
+if __name__ == '__main__':
+    is_alive = True
+    puller_thread = Thread(target=server_pull, args=(is_alive, ))
+    puller_thread.start()
 
-system_type = system()
-info(f'Запущено на {system_type}')
+    system_type = system()
+    info(f'Запущено на {system_type}')
 
-if system_type == 'Darwin':
-    from uis.mac import app
-    app.run()
-else:
-    from uis.windows import run
-    run()
-is_alive = False
+    if system_type == 'Darwin':
+        from uis.mac import app
+        app.run()
+    else:
+        from uis.windows import run
+        run()
+    is_alive = False
--- uis/windows.py
+++ uis/windows.py
@@ -3,14 +3,13 @@ from config import config
 from PIL import Image
 from platform import system
 
+
 def on_clicked(icon, item):
     title = 'mouse' if item.text == 'Мышь' else 'keyboard'
     config.set_value(title, not config.get(title))
 
 
 def run():
-    # TODO: Решить проблемы с отметками
-
     Icon('Quantify', Image.open('assets/icon_win.png' if system() == 'Darwin' else '../assets/icon_win.png'), menu=Menu(
         MenuItem(text='Мышь', action=on_clicked, checked=lambda x: config.get('mouse')),
         MenuItem(text='Клавиатура', action=on_clicked, checked=lambda x: config.get('keyboard'))
