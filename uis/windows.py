cbffcb04e6921a4df211cf26f462eec32e0f111d
breitburg <contact@breitburg.me>
2019-07-18 00:22:24 +0300

    Added dynamic path definition

--- uis/windows.py
+++ uis/windows.py
@@ -1,7 +1,7 @@
 from pystray import Icon, Menu, MenuItem
 from config import config
 from PIL import Image
-
+from platform import system
 
 def on_clicked(icon, item):
     title = 'mouse' if item.text == 'Мышь' else 'keyboard'
@@ -11,7 +11,7 @@ def on_clicked(icon, item):
 def run():
     # TODO: Решить проблемы с отметками
 
-    Icon('Quantify', Image.open('assets/icon_win.png'), menu=Menu(
+    Icon('Quantify', Image.open('assets/icon_win.png' if system() == 'Darwin' else '../assets/icon_win.png'), menu=Menu(
         MenuItem(text='Мышь', action=on_clicked, checked=lambda x: config.get('mouse')),
         MenuItem(text='Клавиатура', action=on_clicked, checked=lambda x: config.get('keyboard'))
     )).run()
