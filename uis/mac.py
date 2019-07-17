5d11b4213b59f872b1998bf55287fc58233fe749
breitburg <contact@breitburg.me>
2019-07-17 13:55:25 +0300

    Fixing mac and changing icon color

--- uis/mac.py
+++ uis/mac.py
@@ -7,6 +7,9 @@ class StatusBarApp(App):
         super().__init__('Quantify', icon='assets/icon_mac.png', quit_button='Выход')
         self.menu = ['Мышь', 'Клавиатура']
 
+        for item in self.menu.items():
+            item[1].state = 1
+
     @clicked('Мышь')
     def mouse(self, sender):
         sender.state = not sender.state
