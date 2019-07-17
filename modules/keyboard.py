4dcdb60ca0c90444b24bf18f28d1385b2c681dfb
StarikTenger <48261272+StarikTenger@users.noreply.github.com>
2019-07-17 17:52:39 +0300

    Keyboard modified

--- modules/keyboard.py
+++ modules/keyboard.py
@@ -4,14 +4,12 @@ from time import time, sleep
 
 key_events = []
 
-
-def key_tracking():
-    global key_events
+def key_tracking(key_events):
     with Listener(on_press=lambda key: key_events.append({"time": time(), "button": key})) as listener:
         listener.join()
 
 
-thread = Thread(target=key_tracking)
+thread = Thread(target=key_tracking, args=(key_events, ))
 thread.start()
 
 while True:
