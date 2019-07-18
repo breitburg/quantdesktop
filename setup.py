7472bb1dc8225fc3c1c8154ff95fb10b928343d1
breitburg <contact@breitburg.me>
2019-07-18 12:58:50 +0300

    Added requirements to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='quantify-desktop',
     version='1.0',
     description='Слежка за пользователями с их согласия',
     long_description=open('readme.md').read(),
-    packages=['desktop']
+    packages=['desktop'],
+    install_requires=[
+        'requests',
+        'pynput',
+        'pystray'
+      ],
 )
