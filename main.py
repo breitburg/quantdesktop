64c062011dcc08896bc0ccc6d627ae8faccc06ee
breitburg <contact@breitburg.me>
2019-07-18 11:17:08 +0300

    Removed Mac code

--- main.py
+++ main.py
@@ -44,8 +44,7 @@ if __name__ == '__main__':
 
     # В зависимости от системы запускаем разные
     # реализации тулбар меню
-    if system_type == 'Darwin': from uis.mac import app
-    else: from uis.universal import app
+    from uis.universal import app
     app.run()
 
     # После завершения работы тулбар
